1f5d527fcbf78fd1db39a25821e799977cdb4048
HATGATE\vixle <vixlemas@gmail.com>
2018-05-15 17:53:40 +0100

    Step 3 - change to ignore comments and other non matching lines

--- test/test-version-bump-reset.py
+++ test/test-version-bump-reset.py
@@ -5,7 +5,16 @@ try:
 except FileNotFoundError:
     print('File not found')
 
-with open('..\VERSION.txt', 'w') as v:
-    v.write('10.345.-SNAPSHOT')
-        
+with open('..\VERSION.txt', 'w', encoding='utf-8') as v:
+    v.write('#a comment\n')
+    v.write('\n')
+    v.write('#that was a blank space\n')
+    v.write('#2.0.6-SNAPSHOT\n')
+    v.write('#What to do with this one?\n')
+    v.write('<version>10.345.54-SNAPSHOT<\\version>\n')
+    v.write('10.345.54-SNAPSHOT\n')
+    v.write('11.4.6-SNAPSHOT\n')
+    v.write('#THE SCRIPT SHOULD IGNORE THAT ONE\n')
+    
+    
     
--- version-bump.py
+++ version-bump.py
@@ -1,23 +1,41 @@
 #this is the script
 import os
 import re
-
+#TODO What happens if writing fails half way through?
+#TODO Compare old and new version.txt
 p = re.compile('^(\d+\.)?(\d+\.)?(\d+\-SNAPSHOT)$')
-with open('VERSION.txt', 'r') as v, open('NEWVERSION.txt', 'w') as nv:
-    read_data = v.readline()
-    #Check for string matching vesion pattern
-    m = p.match(read_data)
-    #check for '-SNAPSHOT' and remove
-    #if (read_data.endswith('-SNAPSHOT')):
-    if m:
-        print('found it: ' + read_data)
-        nv.write(read_data[:-9])
-    else:
-        print('not here: ' + read_data)
+foundit = False
+with open('VERSION.txt', 'r', encoding='utf-8') as v, open('NEWVERSION.txt', 'w', encoding='utf-8') as nv:
+    for read_data in v:
+        #Check for string matching vesion pattern
+        m = p.match(read_data)
+        #check for '-SNAPSHOT' and remove, don't forget the newline character
+        if foundit:
+
+            print('write the rest: ' + read_data)
+            nv.write(read_data)
+        else:
+            if m:
+                print('found it: ' + read_data)
+                nv.write(read_data[:-10]+'\n')
+                print('now just ignore the rest of the file')
+                foundit = True
+            else:
+                print('not here: ' + read_data)
+                nv.write(read_data)
+        #Found a match so now just ignore the rest of the file
+         
     #delete old file and rename new one
     v.close()
     nv.close()
-    os.remove('VERSION.txt')
+    try:
+        os.remove('OLDVERSION.txt')
+    except FileNotFoundError:
+        print('OLDVERSION.txt not found')
+        
+    os.rename('VERSION.txt', 'OLDVERSION.txt')
     os.rename('NEWVERSION.txt', 'VERSION.txt')
+
+    
         
     
