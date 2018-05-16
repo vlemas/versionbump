fe4c4b0aae09ca687d6858e4a296cf4b796877a8
HATGATE\vixle <vixlemas@gmail.com>
2018-05-16 14:47:41 +0100

    Step 4 - change to work for specified file name

--- test/test-version-bump-reset.py
+++ test/test-version-bump-reset.py
@@ -1,11 +1,13 @@
 #a script to reset the test file
 import os
+filename = '.\VERSIONsss.txt'
+print('Recreating '+filename+' for version-bump.py')
 try:
-    os.remove('..\VERSION.txt')
+    os.remove(filename)
 except FileNotFoundError:
-    print('File not found')
+    print('could not find and delete ' + filename + '. Will create new file')
 
-with open('..\VERSION.txt', 'w', encoding='utf-8') as v:
+with open(filename, 'w', encoding='utf-8') as v:
     v.write('#a comment\n')
     v.write('\n')
     v.write('#that was a blank space\n')
@@ -15,6 +17,7 @@ with open('..\VERSION.txt', 'w', encoding='utf-8') as v:
     v.write('10.345.54-SNAPSHOT\n')
     v.write('11.4.6-SNAPSHOT\n')
     v.write('#THE SCRIPT SHOULD IGNORE THAT ONE\n')
+print('done')
     
     
     
--- version-bump.py
+++ version-bump.py
@@ -1,40 +1,64 @@
 #this is the script
 import os
 import re
+import sys
+from optparse import OptionParser
 #TODO What happens if writing fails half way through?
 #TODO Compare old and new version.txt
 p = re.compile('^(\d+\.)?(\d+\.)?(\d+\-SNAPSHOT)$')
 foundit = False
-with open('VERSION.txt', 'r', encoding='utf-8') as v, open('NEWVERSION.txt', 'w', encoding='utf-8') as nv:
-    for read_data in v:
-        #Check for string matching vesion pattern
-        m = p.match(read_data)
-        #check for '-SNAPSHOT' and remove, don't forget the newline character
-        if foundit:
 
-            print('write the rest: ' + read_data)
-            nv.write(read_data)
-        else:
-            if m:
-                print('found it: ' + read_data)
-                nv.write(read_data[:-10]+'\n')
-                print('now just ignore the rest of the file')
-                foundit = True
-            else:
-                print('not here: ' + read_data)
+# find filename
+filename = 'VERSION.txt'
+#argv = sys.argv
+#print('Argument List:', str(argv))
+
+parser = OptionParser()
+parser.add_option("-f", "--file", dest='fname', help='name of the version text file')
+(opts, args) = parser.parse_args()
+if opts.fname:
+    filename = opts.fname
+print('Updating version file: '+filename)
+
+       
+try:  
+    with open(filename, 'r', encoding='utf-8') as v, open('NEW'+filename, 'w', encoding='utf-8') as nv:
+        print('write data to '+'NEW'+filename)
+        for read_data in v:
+            #Check for string matching vesion pattern
+            m = p.match(read_data)
+            if foundit:
+            #Found a match so now just ignore the rest of the file
                 nv.write(read_data)
-        #Found a match so now just ignore the rest of the file
-         
-    #delete old file and rename new one
-    v.close()
-    nv.close()
-    try:
-        os.remove('OLDVERSION.txt')
-    except FileNotFoundError:
-        print('OLDVERSION.txt not found')
-        
-    os.rename('VERSION.txt', 'OLDVERSION.txt')
-    os.rename('NEWVERSION.txt', 'VERSION.txt')
+            else:
+                if m:
+                    newversion=read_data[:-10]
+                    nv.write(newversion+'\n')
+                    #remove '-SNAPSHOT', don't forget the newline character
+                    print('Found version! '+read_data[:-1])
+                    print('Changed to '+read_data[:-10])
+                    print('now just ignore the rest of the file')
+                    foundit = True
+                else:
+                    #not found a match yet so just ignore line
+                    nv.write(read_data)
+             
+        #delete old file and rename new one
+        v.close()
+        nv.close()
+    
+        try:
+            os.remove('OLD'+filename)
+            print('deleted '+'OLD'+filename)
+        except FileNotFoundError:
+            print('OLD'+filename+' not found')
+            
+        os.rename(filename, 'OLD'+filename)
+        print('renamed '+filename+' to '+'OLD'+filename)
+        os.rename('NEW'+filename, filename)
+        print('renamed '+'NEW'+filename+' to '+filename)
+except FileNotFoundError:
+    print(filename+' not found, no action taken')
 
     
         
