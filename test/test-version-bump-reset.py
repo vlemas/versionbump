d98f6b6eb14af2130ec67de16bc473a5ed9dd1cc
HATGATE\vixle <vixlemas@gmail.com>
2018-06-18 11:31:13 +0100

    Tidied up test file creation

--- test/test-version-bump-reset.py
+++ test/test-version-bump-reset.py
@@ -1,13 +1,23 @@
 #a script to reset the test file
 import os
 from optparse import OptionParser
+
 filename = '.\VERSION.txt'
+nextversion = '10.345.54-SNAPSHOT'
+
 parser = OptionParser()
 parser.add_option("-f", "--file", dest='fname', help='name of the version text file')
+parser.add_option("-v", "--version", dest='newversion', help='new version number to be added to the file')
 (opts, args) = parser.parse_args()
+
 if opts.fname:
     filename = opts.fname
 print('Recreating '+filename+' for version-bump.py')
+
+if opts.newversion:
+    nextversion = opts.newversion
+print("Using version "+nextversion)
+
 try:
     os.remove(filename)
 except FileNotFoundError:
@@ -20,7 +30,8 @@ with open(filename, 'w', encoding='utf-8') as v:
     v.write('#2.0.6-SNAPSHOT\n')
     v.write('#What to do with this one?\n')
     v.write('<version>10.345.54-SNAPSHOT<\\version>\n')
-    v.write('10.345.54-SNAPSHOT\n')
+    if nextversion:
+        v.write(nextversion+"\n")
     v.write('11.4.6-SNAPSHOT\n')
     v.write('#THE SCRIPT SHOULD IGNORE THAT ONE\n')
 print('done')
